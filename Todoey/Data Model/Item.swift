a8c03c5f6d93d4d84bc174b070f025444601e949
Avnish Asthana <avnishman@gmail.com>
2019-06-16 16:28:11 -0600

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Avnish Asthana on 6/15/19.
+//  Copyright © 2019 Avnish Asthana. All rights reserved.
+//
+
+import Foundation
+
+class Item
+{
+    var title : String = ""
+    var done : Bool = false
+}
